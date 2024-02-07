935b2e6cbd49e062b61612d2ef9866aa092d1605
Shukurjon2002 <abduvahobovshukurjon0gmail.com>
2024-02-07 15:26:15 +0500

    news

--- js/main.js
+++ js/main.js
@@ -9,3 +9,30 @@ closeBtnModal.addEventListener("mouseup", (e) => {
   signModal.classList.remove("openSignModal");
   signModalBody.classList.remove("openSignBody");
 });
+
+
+
+
+// Kataloggggg
+
+
+var openModalBtn = document.getElementById("openModalBtn");
+
+var modal = document.getElementById("catalogModal");
+
+var closeBtn = document.getElementsByClassName("close")[0];
+
+openModalBtn.onclick = function() {
+  modal.style.display = "block";
+}
+
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
